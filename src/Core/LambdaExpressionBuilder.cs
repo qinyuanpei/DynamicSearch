42a3cbb75884daa4b8a07101946648a0f5252f26
qinyuanpei <qinyuanpei@163.com>
2021-01-05 22:01:24 +0800

    initial commit

---
+++ src/Core/LambdaExpressionBuilder.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Collections;
+using System.Linq.Expressions;
+using System.Collections.Generic;
+
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace DynamicSearch.Core
+{
+    public static class LambdaExpressionBuilder
+    {
+        private static Expression GetExpression(ParameterExpression parameter, Condition condition)
+        {
+            var propertyParam = Expression.Property(parameter, condition.Field);
+
+            var propertyInfo = propertyParam.Member as PropertyInfo;
+            if (propertyInfo == null) 
+                throw new ArgumentException($"Invalid property \"{condition.Field}\"");
+
+            var realPropertyType = Nullable.GetUnderlyingType(propertyInfo.PropertyType) ?? propertyInfo.PropertyType;
+            if (condition.Op != Operation.StdIn && condition.Op != Operation.StdNotIn)
+                condition.Value = Convert.ChangeType(condition.Value, realPropertyType);
+            var constantParam = Expression.Constant(condition.Value);
+            switch (condition.Op)
+            {
+                case Operation.Equals:
+                    return Expression.Equal(propertyParam, constantParam);
+                case Operation.NotEquals:
+                    return Expression.NotEqual(propertyParam, constantParam);
+                case Operation.Contains:
+                    return Expression.Call(propertyParam, "Contains", null, constantParam); ;
+                case Operation.NotContains:
+                    return Expression.Not(Expression.Call(propertyParam, "Contains", null, constantParam));
+                case Operation.StartsWith:
+                    return Expression.Call(propertyParam, "StartsWith", null, constantParam);
+                case Operation.EndsWith:
+                    return Expression.Call(propertyParam, "EndsWith", null, constantParam);
+                case Operation.GreaterThen:
+                    return Expression.GreaterThan(propertyParam, constantParam);
+                case Operation.GreaterThenOrEquals:
+                    return Expression.GreaterThanOrEqual(propertyParam, constantParam);
+                case Operation.LessThan:
+                    return Expression.LessThan(propertyParam, constantParam);
+                case Operation.LessThanOrEquals:
+                    return Expression.LessThanOrEqual(propertyParam, constantParam);
+                case Operation.StdIn:
+                    return Expression.Call(typeof(Enumerable), "Contains",new Type[] { realPropertyType }, new Expression[] { constantParam, propertyParam });
+                case Operation.StdNotIn:
+                    return Expression.Not(Expression.Call(typeof(Enumerable), "Contains", new Type[] { realPropertyType }, new Expression[] { constantParam, propertyParam }));
+            }
+
+            return null;
+        }
+
+        private static Expression GetGroupExpression(ParameterExpression parameter, List<Condition> orConditions)
+        {
+            if (orConditions.Count == 0)
+                return null;
+
+            var exps = orConditions.Select(c => GetExpression(parameter, c)).ToList();
+            return exps.Aggregate<Expression, Expression>(null, (left, right) =>
+            {
+                if (left == null)
+                    return right;
+                return Expression.OrElse(left, right);
+            });
+        }
+
+        public static Expression<Func<T, bool>> BuildLambda<T>(IEnumerable<Condition> conditions)
+        {
+            if (conditions == null || !conditions.Any()) return x => true;
+            var parameter = Expression.Parameter(typeof(T), "x");
+
+            //简单条件
+            var simpleExps = conditions
+                .ToList()
+                .FindAll(c => string.IsNullOrEmpty(c.OrGroup))
+                .Select(c => GetExpression(parameter, c))
+                .ToList();
+
+            //复杂条件
+            var complexExps = conditions
+                .ToList()
+                .FindAll(c => !string.IsNullOrEmpty(c.OrGroup))
+                .GroupBy(x => x.OrGroup)
+                .Select(g => GetGroupExpression(parameter, g.ToList()))
+                .ToList();
+
+            var exp = simpleExps.Concat(complexExps).Aggregate<Expression, Expression>(null, (left, right) =>
+            {
+                if (left == null)
+                    return right;
+                return Expression.AndAlso(left, right);
+            }); ;
+            return Expression.Lambda<Func<T, bool>>(exp, parameter);
+        }
+
+        public static Expression<Func<T, bool>> BuildAndAlsoLambda<T>(IEnumerable<Condition> conditions)
+        {
+            if (conditions == null || !conditions.Any()) return x => true;
+            var parameter = Expression.Parameter(typeof(T), "x");
+            var simpleExps = conditions
+                .ToList()
+                .Select(c=>GetExpression(parameter, c))
+                .ToList();
+
+            var exp = simpleExps.Aggregate<Expression, Expression>(null, (left, right) => {
+                if (left == null)
+                    return right;
+                return Expression.AndAlso(left, right);
+            });
+            return Expression.Lambda<Func<T, bool>>(exp, parameter);
+
+        }
+
+        public static Expression<Func<T,bool>> BuildOrElseLambda<T>(IEnumerable<Condition> conditions)
+        {
+            if (conditions == null || !conditions.Any()) return x => true;
+            var parameter = Expression.Parameter(typeof(T), "x");
+            var simpleExps = conditions
+                .ToList()
+                .Select(c=>GetExpression(parameter, c))
+                .ToList();
+
+            var exp = simpleExps.Aggregate<Expression, Expression>(null, (left, right) => {
+                if (left == null)
+                    return right;
+                return Expression.OrElse(left, right);
+            });
+            return Expression.Lambda<Func<T, bool>>(exp, parameter);
+        }
+    }
+
+}
---
+++ src/Core/SearchParameters.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections;
+using System.Reflection.Emit;
+using System.Linq.Expressions;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+
+namespace DynamicSearch.Core
+{
+
+    public class SearchParameters
+    {
+        public PageModel Page { get; set; }
+        public QueryModel Query { get; set; }
+    }
+
+    public class QueryModel : List<Condition>
+    {
+        public void Add<T>(Condition<T> condition) where T : class
+        {
+            var filedName = string.Empty;
+            var memberExp = condition.Field.Body as MemberExpression;
+            if (memberExp == null)
+            {
+                var ubody = (UnaryExpression)condition.Field.Body;
+                memberExp = ubody.Operand as MemberExpression;
+            }
+            filedName = memberExp.Member.Name;
+            Add(new Condition() { Field = filedName, Op = condition.Op, Value = condition.Value, OrGroup = condition.OrGroup });
+        }
+    }
+
+    public class PageModel
+    {
+        public int CurrentPage { get; set; } = 1;
+        public int PageSize { get; set; } = 500;
+        public SortType SortType { get; set; }
+        public List<string> SortFields { get; set; }
+    }
+
+    public enum SortType
+    {
+        Asc = 10,
+        Desc = 20
+    }
+
+    public enum Operation
+    {
+        Equals = 10,
+        NotEquals = 20,
+        LessThan = 30,
+        LessThanOrEquals = 40,
+        GreaterThen = 50,
+        GreaterThenOrEquals = 60,
+        StdIn = 70,
+        StdNotIn = 80,
+        Contains = 90,
+        NotContains = 100,
+        StartsWith = 110,
+        EndsWith = 120
+    }
+
+    public class Condition
+    {
+        public string Field { get; set; }
+        public Operation Op { get; set; }
+        public object Value { get; set; }
+        public string OrGroup { get; set; }
+    }
+
+    public class Condition<T> : Condition
+    {
+        public new Expression<Func<T, dynamic>> Field { get; set; }
+    }
+}
